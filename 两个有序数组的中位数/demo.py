356828fb3fc5056eb20d73f73a8daf913e7ca9b1
JiaxuHan <31310679+githubhjx@users.noreply.github.com>
2019-08-22 16:14:57 +0800

    Update demo.py

--- 两个有序数组的中位数/demo.py
+++ 两个有序数组的中位数/demo.py
@@ -35,37 +35,41 @@ class Solution:
 # 递归法，数学容易证明，将两个数组取二分，得到两个中间值，共四段，
 # 则两个数组的中位数必然在小的中间值后半段和大的中间值的前半段，递归剩下两段
 class Solution:
-    def findMedianSortedArrays(self, nums1: A[int], nums2: B[int]) -> float:
-        int m = A.length;
-        int n = B.length;
-        if (m > n) { 
-            return findMedianSortedArrays(B,A); // 保证 m <= n
-        }
-        int iMin = 0, iMax = m;
-        while (iMin <= iMax) {
-            int i = (iMin + iMax) / 2;
-            int j = (m + n + 1) / 2 - i;
-            if (j != 0 and i != m and B[j-1] > A[i]){ // i 需要增大
-                iMin = i + 1; 
-            }
-            else if (i != 0 and j != n and A[i-1] > B[j]) { // i 需要减小
-                iMax = i - 1; 
-            }
-            else { // 达到要求，并且将边界条件列出来单独考虑
-                int maxLeft = 0;
-                if (i == 0) { maxLeft = B[j-1]; }
-                else if (j == 0) { maxLeft = A[i-1]; }
-                else { maxLeft = Math.max(A[i-1], B[j-1]); }
-                if ( (m + n) % 2 == 1 ) { return maxLeft; } // 奇数的话不需要考虑右半部分
+    def findMedianSortedArrays(self, A, B) -> float:
+        m = len(A)
+        n = len(B)
+        if m > n:
+            return findMedianSortedArrays(self, B, A)   # 保证 m <= n
 
-                int minRight = 0;
-                if (i == m) { minRight = B[j]; }
-                else if (j == n) { minRight = A[i]; }
-                else { minRight = Math.min(B[j], A[i]); }
+        iMin = 0
+        iMax = m
+        while (iMin <= iMax):
+            i = (iMin + iMax) / 2
+            j = (m + n + 1) / 2 - i
+            if j != 0 and i != m and B[j-1] > A[i]:     # i 需要增大
+                iMin = i + 1
+            
+            elif i != 0 and j != n and A[i-1] > B[j]:   # i 需要减小
+                iMax = i - 1
+            
+            else:  # 达到要求，并且将边界条件列出来单独考虑
+                maxLeft = 0
+                if i == 0:
+                    maxLeft = B[j-1]
+                elif j == 0:
+                    maxLeft = A[i-1]
+                else:
+                    maxLeft = max(A[i-1], B[j-1])
+                if (m + n) % 2 == 1: 
+                    return maxLeft  # 奇数的话不需要考虑右半部分
 
-                return (maxLeft + minRight) / 2.0; //如果是偶数的话返回结果
-            }
-        }
-        return 0.0;
-    }
-}
+                minRight = 0
+                if i == m:
+                    minRight = B[j]
+                elif j == n:
+                    minRight = A[i]
+                else:
+                    minRight = min(B[j], A[i])
+
+                return (maxLeft + minRight) / 2.0   # 如果是偶数的话返回结果
+        return 0
