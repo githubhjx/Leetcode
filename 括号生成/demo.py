b678a441a8cab6c98e51e363b7c798a0a587e533
JiaxuHan <31310679+githubhjx@users.noreply.github.com>
2019-08-31 09:58:46 +0800

    Create demo.py

---
+++ 括号生成/demo.py
@@ -0,0 +1,94 @@
+"""
+给出 n 代表生成括号的对数，请你写出一个函数，使其能够生成所有可能的并且有效的括号组合。
+
+例如，给出 n = 3，生成结果为：
+
+[
+  "((()))",
+  "(()())",
+  "(())()",
+  "()(())",
+  "()()()"
+]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/generate-parentheses
+"""
+
+# 暴力法
+class Solution(object):
+    def generateParenthesis(self, n):
+        def generate(A = []):
+            if len(A) == 2*n:
+                if valid(A):
+                    ans.append("".join(A))
+            else:
+                A.append('(')
+                generate(A)
+                A.pop()
+                A.append(')')
+                generate(A)
+                A.pop()
+
+        def valid(A):
+            bal = 0
+            for c in A:
+                if c == '(': bal += 1
+                else: bal -= 1
+                if bal < 0: return False
+            return bal == 0
+
+        ans = []
+        generate()
+        return ans
+
+# 回溯法
+class Solution(object):
+    def generateParenthesis(self, N):
+        ans = []
+        def backtrack(S = '', left = 0, right = 0):
+            if len(S) == 2 * N:
+                ans.append(S)
+                return
+            if left < N:
+                backtrack(S+'(', left+1, right)
+            if right < left:
+                backtrack(S+')', left, right+1)
+
+        backtrack()
+        return ans
+
+# 闭合数
+class Solution(object):
+    def generateParenthesis(self, N):
+        if N == 0: return ['']
+        ans = []
+        for c in xrange(N):
+            for left in self.generateParenthesis(c):
+                for right in self.generateParenthesis(N-1-c):
+                    ans.append('({}){}'.format(left, right))
+        return ans
+
+# 动态规划
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        if n == 0:
+            return []
+        total_l = []
+        total_l.append([None])    # 0组括号时记为None
+        total_l.append(["()"])    # 1组括号只有一种情况
+        for i in range(2,n+1):    # 开始计算i组括号时的括号组合
+            l = []        
+            for j in range(i):    # 开始遍历 p q ，其中p+q=n-1 , j 作为索引
+                now_list1 = total_l[j]    # p = j 时的括号组合情况
+                now_list2 = total_l[i-1-j]    # q = (i-1) - j 时的括号组合情况
+                for k1 in now_list1:  
+                    for k2 in now_list2:
+                        if k1 == None:
+                            k1 = ""
+                        if k2 == None:
+                            k2 = ""
+                        el = "(" + k1 + ")" + k2
+                        l.append(el)    # 把所有可能的情况添加到 l 中
+            total_l.append(l)    # l这个list就是i组括号的所有情况，添加到total_l中，继续求解i=i+1的情况
+        return total_l[n]
